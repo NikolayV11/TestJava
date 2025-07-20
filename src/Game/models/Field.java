ae27b3809f5ae3dd534ca8982c1315f9696d8e9b
Николай <wasilev-n@mail.ru>
2025-07-20 19:05:30 +0500

    Filed Проверка на пустую ячейку куда мы хотим записать свой символ.

--- src/Game/models/Field.java
+++ src/Game/models/Field.java
@@ -46,6 +46,11 @@ public class Field {
 
 	public void setCellField(int i, int j, String symbol){
 		if(getSIZE_FIELD() -1 <= i || getSIZE_FIELD() - 1 <= j)return;
-		stateField[i][j] = symbol;
+		if(stateField[i][j] == DEFAULT_SYMBOL || stateField[i][j] == null) {
+			this.stateField[i][j] = symbol;
+		} else {
+			System.out.println("Поле занято");
+		}
+
 	}
 }
