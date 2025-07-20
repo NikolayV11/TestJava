6a03edc3c7f72142947e8e72b623a50103db6655
Николай <wasilev-n@mail.ru>
2025-07-20 19:28:55 +0500

    Player Создание игрока

---
+++ src/Game/models/Player.java
@@ -0,0 +1,19 @@
+package Game.models;
+
+public class Player {
+	private final String NAME;
+	private final String FIGURE;
+
+	public Player(String NAME, String FIGURE) {
+		this.NAME = NAME;
+		this.FIGURE = FIGURE;
+	}
+
+	public String getNAME() {
+		return NAME;
+	}
+
+	public String getFIGURE() {
+		return FIGURE;
+	}
+}
